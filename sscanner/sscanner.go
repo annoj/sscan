0291ae30ca1fcb343e4f1c9d1b6a4ee0a24799d1
Jona <s_heitzer@stud.hwr-berlin.de>
2019-11-11 22:19:42 +0100

    Make Scanner write results to file

--- sscanner/sscanner.go
+++ sscanner/sscanner.go
@@ -1,4 +1,4 @@
-//
+
 // sscann subdomain scanner
 // ========================
 //
@@ -20,15 +20,16 @@ import (
 )
 
 type Scanner struct {
-	domain         string
-	subdomainsFile *os.File
-	resolver       string
+	domain			string
+	subdomainsFile	*os.File
+	resolver		string
+	outfilePath		string
 }
 
 func readSubdomainsFile(fp string) *os.File {
 	f, err := os.Open(fp)
 	if err != nil {
-		panic("Subdomainsfile could not be opened")
+		log.Panic("Subdomainsfile could not be opened")
 	}
 	return f
 }
@@ -37,30 +38,69 @@ func (s *Scanner) Init(
 	domain string,
 	subdomainsFilePath string,
 	resolver string,
+	outfilePath string,
 ) {
+	log.Println("Initializing scanner")
 	s.domain = domain
 	s.subdomainsFile = readSubdomainsFile(subdomainsFilePath)
 	s.resolver = resolver
+	s.outfilePath = outfilePath
 }
 
-func lookup(domain string, wg *sync.WaitGroup) {
+func lookup(
+	domain string,
+	wg *sync.WaitGroup,
+	outChan chan string,
+	done chan bool,
+) {
 	defer wg.Done()
 	ips, err := net.LookupHost(domain)
 	if err == nil {
 		for _, ip := range ips {
 			log.Printf("Found %s at %s", domain, ip)
+			outChan <- fmt.Sprintf("%s,%s", domain, ip)
 		}
 	}
 }
 
+func writeResultsToFile(
+	outfilePath string,
+	results chan string,
+	done chan bool,
+) {
+	f, err := os.Create(outfilePath)
+	defer f.Close()
+	if err != nil {
+		log.Panic("Could not open outfile!")
+	}
+	for res := range results {
+		_, err = fmt.Fprintln(f, res)
+		if err != nil {
+			log.Print(err)
+		}
+	}
+	done <- true
+}
+
 func (s *Scanner) Scan() {
 	var wg sync.WaitGroup
+	outChan := make(chan string)
+	done := make(chan bool)
 	scanner := bufio.NewScanner(s.subdomainsFile)
+	log.Println("Scan running...")
 	for scanner.Scan() {
 		subdomain := fmt.Sprintf("%s.%s", scanner.Text(), s.domain)
-		go lookup(subdomain, &wg)
+		go lookup(subdomain, &wg, outChan, done)
 		wg.Add(1)
 	}
 	s.subdomainsFile.Close()
-	wg.Wait()
+	go writeResultsToFile(s.outfilePath, outChan, done)
+	go func() {
+		wg.Wait()
+		close(outChan)
+	}()
+	d := <- done
+	if d == true {
+		log.Println("Done!")
+	}
 }
